97486588d128934cca76e84faaaafc626c79070e
oomuro <t-oomuro@winas.jp>
2019-01-09 10:44:49 +0900

    Initial Commit

---
+++ ChatGroupLayout-Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatGroupLayout-Practice
+//
+//  Created by 大室 on 2019/01/09.
+//  Copyright © 2019年 大室. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
