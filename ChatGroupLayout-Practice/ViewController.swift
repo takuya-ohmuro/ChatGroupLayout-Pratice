9c46e6f9cffadfc2ddbfa396887b9ca524f1a6a0
oomuro <t-oomuro@winas.jp>
2019-01-09 10:51:51 +0900

    first commit

--- ChatGroupLayout-Practice/ViewController.swift
+++ ChatGroupLayout-Practice/ViewController.swift
@@ -8,13 +8,187 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct ChatMessage {
+    let text: String
+    let isIncoming: Bool
+    let date: Date
+}
+
+extension Date {
+    static func dateFromCustomString(customString: String) -> Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        return dateFormatter.date(from: customString) ?? Date()
+    }
+
+    func reduceToMonthDayYear() -> Date {
+        let calendar = Calendar.current
+        let month = calendar.component(.month, from: self)
+        let day = calendar.component(.day, from: self)
+        let year = calendar.component(.year, from: self)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        return dateFormatter.date(from: "\(month)/\(day)/\(year)") ?? Date()
+    }
+}
+
+class ViewController: UITableViewController {
+
+    fileprivate let cellId = "id123"
+
+    //    let chatMessages = [
+    //        [
+    //            ChatMessage(text: "Here's my very first message", isIncoming: true, date: Date.dateFromCustomString(customString: "08/03/2018")),
+    //            ChatMessage(text: "I'm going to message another long message that will word wrap", isIncoming: true, date: Date.dateFromCustomString(customString: "08/03/2018")),
+    //        ],
+    //        [
+    //            ChatMessage(text: "I'm going to message another long message that will word wrap, I'm going to message another long message that will word wrap, I'm going to message another long message that will word wrap", isIncoming: false, date: Date.dateFromCustomString(customString: "09/15/2018")),
+    //            ChatMessage(text: "Yo, dawg, Whaddup!", isIncoming: false, date: Date()),
+    //            ChatMessage(text: "This message should appear on the left with a white background bubble", isIncoming: true, date: Date.dateFromCustomString(customString: "09/15/2018")),
+    //        ],
+    //        [
+    //            ChatMessage(text: "Third Section message", isIncoming: true, date: Date.dateFromCustomString(customString: "10/31/2018"))
+    //        ]
+    //    ]
+
+    let messagesFromServer = [
+        ChatMessage(text: "Here's my very first message", isIncoming: true, date: Date.dateFromCustomString(customString: "08/03/2018")),
+        ChatMessage(text: "I'm going to message another long message that will word wrap", isIncoming: true, date: Date.dateFromCustomString(customString: "08/03/2018")),
+        ChatMessage(text: "I'm going to message another long message that will word wrap, I'm going to message another long message that will word wrap, I'm going to message another long message that will word wrap", isIncoming: false, date: Date.dateFromCustomString(customString: "09/15/2018")),
+        ChatMessage(text: "初めまして", isIncoming: false, date: Date()),
+        ChatMessage(text: "This message should appear on the left with a white background bubble", isIncoming: true, date: Date.dateFromCustomString(customString: "09/15/2018")),
+        ChatMessage(text: "こんにちは", isIncoming: true, date: Date.dateFromCustomString(customString: "10/31/2018"))
+    ]
+
+    fileprivate func attemptToAssembleGroupedMessages() {
+        print("Attempt to group our messages together based on Date property")
+
+        let groupedMessages = Dictionary(grouping: messagesFromServer) { (element) -> Date in
+            return element.date.reduceToMonthDayYear()
+        }
+
+        // provide a sorting for your keys somehow
+        let sortedKeys = groupedMessages.keys.sorted()
+        sortedKeys.forEach { (key) in
+            let values = groupedMessages[key]
+            chatMessages.append(values ?? [])
+        }
+
+        //        groupedMessages.keys.forEach { (key) in
+        //            print(key)
+        //            let values = groupedMessages[key]
+        //            print(values ?? "")
+        //
+        //            chatMessages.append(values ?? [])
+        //        }
+
+        //        chatMessages = groupedMessages.values
+
+        //        print(groupedMessages)
+    }
+
+    var chatMessages = [[ChatMessage]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        attemptToAssembleGroupedMessages()
+
+        navigationItem.title = "Messages"
+        navigationController?.navigationBar.prefersLargeTitles = true
+
+        tableView.register(ChatMessageCell.self, forCellReuseIdentifier: cellId)
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = UIColor(white: 0.95, alpha: 1)
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return chatMessages.count
+    }
+
+    class DateHeaderLabel: UILabel {
+
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+
+            backgroundColor = .black
+            textColor = .white
+            textAlignment = .center
+            translatesAutoresizingMaskIntoConstraints = false // enables auto layout
+            font = UIFont.boldSystemFont(ofSize: 14)
+        }
+
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+
+        override var intrinsicContentSize: CGSize {
+            let originalContentSize = super.intrinsicContentSize
+            let height = originalContentSize.height + 12
+            layer.cornerRadius = height / 2
+            layer.masksToBounds = true
+            return CGSize(width: originalContentSize.width + 20, height: height)
+        }
+
+    }
+
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if let firstMessageInSection = chatMessages[section].first {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "MM/dd/yyyy"
+            let dateString = dateFormatter.string(from: firstMessageInSection.date)
+
+            let label = DateHeaderLabel()
+            label.text = dateString
+
+            let containerView = UIView()
+
+            containerView.addSubview(label)
+            label.centerXAnchor.constraint(equalTo: containerView.centerXAnchor).isActive = true
+            label.centerYAnchor.constraint(equalTo: containerView.centerYAnchor).isActive = true
+
+            return containerView
+
+        }
+        return nil
+    }
+
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+
+    //    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+
+    //
+    //        return "Section: \(Date())"
+    //    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return chatMessages[section].count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! ChatMessageCell
+        //        let chatMessage = chatMessages[indexPath.row]
+        let chatMessage = chatMessages[indexPath.section][indexPath.row]
+        cell.chatMessage = chatMessage
+        return cell
     }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
